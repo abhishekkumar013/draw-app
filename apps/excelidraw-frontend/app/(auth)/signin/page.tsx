503d8cf95f8b6bf6537cc70bd14fc7952705c873
Abhishek kumar <sahaabhishek443@gmail.com>
2025-04-15 14:43:32 +0530

    new frontend

---
+++ apps/excelidraw-frontend/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+import { AuthPage } from "@/components/AuthPage";
+import React from "react";
+
+const SignInPage = () => {
+  return <AuthPage isSignin={true} />;
+};
+
+export default SignInPage;
---
+++ apps/excelidraw-frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+import { AuthPage } from "@/components/AuthPage";
+import React from "react";
+
+const SignUpPage = () => {
+  return <AuthPage isSignin={false} />;
+};
+
+export default SignUpPage;
